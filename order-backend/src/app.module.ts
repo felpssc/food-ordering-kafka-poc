ae3b746a409dad1b62f92c984dc4f37bafa69b58
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-09-21 22:02:56 -0300

    chore(order-backend): set config to load environment variables

--- order-backend/src/app.module.ts
+++ order-backend/src/app.module.ts
@@ -4,15 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { OrderModule } from './order/order.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://root:root@172.30.0.2:27017/nest?authSource=admin',
-      {
-        useNewUrlParser: true,
-      },
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DSN, {
+      useNewUrlParser: true,
+    }),
     ProductsModule,
     OrderModule,
   ],
