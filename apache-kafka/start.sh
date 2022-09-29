9bdcd9a7143ffee744794dd22b70d79d958c6559
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-09-28 21:39:23 -0300

    refactor(start.sh): change restart to start

--- apache-kafka/start.sh
+++ apache-kafka/start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-docker-compose restart zookeeper
+docker-compose start zookeeper
 sleep 3
-docker-compose restart kafka
+docker-compose start kafka
 sleep 5
-docker-compose restart kafka-topics-generator
+docker-compose start kafka-topics-generator
 sleep 2
-docker-compose restart control-center
+docker-compose start control-center
 sleep 2
-docker-compose restart kafka-connect
+docker-compose start kafka-connect
 sleep 2
 sudo sysctl -w vm.max_map_count=262144
 sleep 1
-docker-compose restart es01
+docker-compose start es01
 sleep 2
-docker-compose restart kibana
+docker-compose start kibana
